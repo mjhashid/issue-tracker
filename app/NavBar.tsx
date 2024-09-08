1a9829e5a207d67d2b6d0532c2a7e48413c16dc4
Momohjimoh Hashidu <hashibibat@gmail.com>
2024-09-08 06:47:40 +0100

    Installed DaisyUI

---
+++ app/NavBar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <Link href="/">Logo</Link>
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  );
+}
