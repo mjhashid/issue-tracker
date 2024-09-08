cef9f130209623b74add29fd3b8f39e0691beb22
Momohjimoh Hashidu <hashibibat@gmail.com>
2024-09-08 14:51:20 +0100

    Adding a bug icon to the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 items-center h-16">
-      <Link href="/">Logo</Link>
+    <nav className="flex space-x-6 border-b mb-5 px-5 items-center h-14">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
       <ul className="flex space-x-6">
         <li>
-          <Link href="/">Dashboard</Link>
+          <Link
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            href="/"
+          >
+            Dashboard
+          </Link>
         </li>
         <li>
           <Link href="/issues">Issues</Link>
